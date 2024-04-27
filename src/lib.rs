37232bb693e083b5854ea80c526453f887a70d3d
慕梓茜 <voidwind@qq.com>
2024-04-27 19:34:45 +0800

    start

--- src/lib.rs
+++ src/lib.rs
@@ -52,3 +52,16 @@ macro_rules! log_link {
         println!("\x1b[36m{}{}\x1b[0m {} \x1b[35m[ 通信 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
     }};
 }
+
+#[macro_export]
+macro_rules! log_msg {
+    ($($arg:tt)*) => {{
+
+        let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
+        let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+
+        let mut output = String::new();
+        write!(&mut output, $($arg)*).unwrap();
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[95m[ 消息 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+    }};
+}
