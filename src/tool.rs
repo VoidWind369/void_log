6cc946a8595963445180421bfb7528db66631ae6
慕梓茜 <voidwind@qq.com>
2024-11-13 14:48:18 +0800

    update to time

--- src/tool.rs
+++ src/tool.rs
@@ -3,14 +3,14 @@ macro_rules! log_info {
     ($($arg:tt)*) => {{
         use std::fmt::Write;
         use std::thread::current;
-        pub use chrono::Local;
 
         let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
         let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+        let now = time::OffsetDateTime::now_utc().to_string();
 
         let mut output = String::new();
         write!(&mut output, $($arg)*).unwrap();
-        println!("\x1b[36m{}{}\x1b[0m {} \x1b[32m[ 正常 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[32m[ 正常 ]\x1b[0m {}", th_name, &th_id, now, output);
     }};
 }
 
@@ -19,14 +19,14 @@ macro_rules! log_warn {
     ($($arg:tt)*) => {{
         use std::fmt::Write;
         use std::thread::current;
-        pub use chrono::Local;
 
         let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
         let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+        let now = time::OffsetDateTime::now_utc().to_string();
 
         let mut output = String::new();
         write!(&mut output, $($arg)*).unwrap();
-        println!("\x1b[36m{}{}\x1b[0m {} \x1b[33m[ 警告 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[33m[ 警告 ]\x1b[0m {}", th_name, &th_id, now, output);
     }};
 }
 
@@ -35,14 +35,14 @@ macro_rules! log_error {
     ($($arg:tt)*) => {{
         use std::fmt::Write;
         use std::thread::current;
-        pub use chrono::Local;
 
         let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
         let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+        let now = time::OffsetDateTime::now_utc().to_string();
 
         let mut output = String::new();
         write!(&mut output, $($arg)*).unwrap();
-        println!("\x1b[36m{}{}\x1b[0m {} \x1b[31m[ 错误 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[31m[ 错误 ]\x1b[0m {}", th_name, &th_id, now, output);
     }};
 }
 
@@ -51,14 +51,14 @@ macro_rules! log_link {
     ($($arg:tt)*) => {{
         use std::fmt::Write;
         use std::thread::current;
-        pub use chrono::Local;
 
         let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
         let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+        let now = time::OffsetDateTime::now_utc().to_string();
 
         let mut output = String::new();
         write!(&mut output, $($arg)*).unwrap();
-        println!("\x1b[36m{}{}\x1b[0m {} \x1b[35m[ 通信 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[35m[ 通信 ]\x1b[0m {}", th_name, &th_id, now, output);
     }};
 }
 
@@ -67,13 +67,13 @@ macro_rules! log_msg {
     ($($arg:tt)*) => {{
         use std::fmt::Write;
         use std::thread::current;
-        pub use chrono::Local;
 
         let th_id = format!("{:?}", current().id()).replace("ThreadId", "线程");
         let th_name = &(current().name().unwrap().to_string() + "0")[0..5];
+        let now = time::OffsetDateTime::now_utc().to_string();
 
         let mut output = String::new();
         write!(&mut output, $($arg)*).unwrap();
-        println!("\x1b[36m{}{}\x1b[0m {} \x1b[95m[ 消息 ]\x1b[0m {}", th_name, &th_id, Local::now().format("%Y-%m-%d %H:%M:%S"), output);
+        println!("\x1b[36m{}{}\x1b[0m {} \x1b[95m[ 消息 ]\x1b[0m {}", th_name, &th_id, now, output);
     }};
 }
